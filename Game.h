7c2160229caea61096b1ea4403db81890a66d48a
lordpsyco <laureanogalvan65@gmail.com>
2026-08-03 17:55:59 -0300

    Quick Update

--- Game.h
+++ Game.h
@@ -1,7 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
 #include "Scene.h"
-#include "Match.h"
 #include <SFML/Graphics/RenderWindow.hpp>
 
 using namespace sf;
--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include "Game.h"
 #include "Match.h"
-int main (int argc, char *argc[]){
+int main(int argc, char *argv[]){
     Game g (new Match);
     g.play();
     return 0;
